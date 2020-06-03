04494b4500ea116155c432f64c0e6ca2967a8064
Geiji <2is4you@gmail.com>
2020-06-02 19:43:17 -0700

    new packages

--- FriendFinderapp/server.js
+++ FriendFinderapp/server.js
@@ -18,4 +18,17 @@ require('./app/routing/htmlRoutes.js')(app);
 // Server listning//
 app.listen(PORT, function(){
 	console.log("App listening on PORT " + PORT);
+
+	const http = require('http'),
+      server = http.createServer();
+
+server.on('request',(request,response)=>{
+   response.writeHead(200,{'Content-Type':'text/plain'});
+   response.write('Hello world');
+   response.end();
+});
+
+server.listen(3000,()=>{
+  console.log('Node server created at port 8080');
+});
 });
